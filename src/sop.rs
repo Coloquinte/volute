ff0744ae513a4938708c6e76f6a5b322a7ec80b5
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-01-06 11:50:15 +0000

    Fix feature documentation and keywords + bump version number

--- src/sop.rs
+++ src/sop.rs
@@ -10,7 +10,7 @@ mod soes;
 mod sop;
 
 #[cfg(any(feature = "optim-mip", feature = "optim-sat"))]
-#[cfg_attr(docsrs, doc(cfg(feature = "optim-mip")))]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "optim-mip", feature = "optim-sat"))))]
 pub mod optim;
 
 pub use cube::Cube;
